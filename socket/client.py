eb37543d7f8b9ae83e18a01659c2f7ac692eaee0
liux120 <43945684+liux120@users.noreply.github.com>
2019-12-09 19:45:11 -0800

    Add files via upload

--- socket/client.py
+++ socket/client.py
@@ -5,14 +5,14 @@ host = socket.gethostname()     # Get local machine name
 port = 60000                    # Reserve a port for your service.
 
 s.connect((host, port))
-s.send("Hello server!")
+s.send("Hello server!".encode())
 
 with open('received.wasm', 'wb') as f:
     print ('file opened')
     while True:
         print('receiving data...')
         data = s.recv(1024)
-        print('data=%s', (data))
+        #print('data=%s', (data))
         if not data:
             break
         # write data to a file
--- socket/server.py
+++ socket/server.py
@@ -19,7 +19,7 @@ while True:
     l = f.read(1024)
     while (l):
        conn.send(l)
-       print('Sent ',repr(l))
+       #print('Sent ',repr(l))
        l = f.read(1024)
     f.close()
 
